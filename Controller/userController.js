7af53d8e2840e49eef745f0c0858f6c2f733914f
shubham8077 <113425042+shubham8077@users.noreply.github.com>
2024-11-08 15:09:04 +0530

    Update userController.js

--- Controller/userController.js
+++ Controller/userController.js
@@ -18,7 +18,7 @@ exports.postUser = async (req, res) => {
         
         const salt = await bcrypt.genSalt(10)
         
-        req.body.password = await bcrypt.hash(req.body.pasword, salt)
+        req.body.password = await bcrypt.hash(req.body.password, salt)
         
         const data = await User.create(req.body)
         return res.json({errors:false,data:data})
